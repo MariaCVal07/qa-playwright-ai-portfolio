40f24684afc0984843fced6a23f0ebbeafe622a8
Maria Camila Valencia <mariacval07@gmail.com>
2026-02-24 17:19:13 -0500

    Update: Files setup

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,10 +3,7 @@ import { LoginPage } from '../pages/login.pages';
 
 test('User can login', async ({ page }) => {
   const loginPage = new LoginPage(page);
-
-  await loginPage.goto('https://www.saucedemo.com/');
+  await page.goto('/');
   await loginPage.login('standard_user', 'secret_sauce');
-
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-
 });
